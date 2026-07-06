0526b2c20e6241806a0c2d6219417748ab14c82d
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2026-07-06 13:52:47 -0500

    Fix fallback mapping detection for 26.x and generation based on fallback mappings
    change many ambiguous variable names
    add test for fallback detection

--- src/main/java/tf/tuff/y0/ViaBlockIds.java
+++ src/main/java/tf/tuff/y0/ViaBlockIds.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
+import javax.annotation.Nonnull;
 
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
@@ -57,10 +58,10 @@ public class ViaBlockIds {
 			if (!p.getDataFolder().exists()) {
 				p.getDataFolder().mkdirs();
 			}
-			generateMappings(mappingsFile);
+			generateMappings();
 		} else {
 			plugin.info("Loading mappings from " + mappingsFile.getName());
-			loadMappings(mappingsFile);
+			loadMappings();
 		}
 	}
 
@@ -94,76 +95,98 @@ public class ViaBlockIds {
 		return "1.21";
 	}
 
-	private InputStream fmf() {
-		String[] vp = serverVersion.split("\\.");
+	public static record MappingFile (String version, InputStream stream) {}
+
+	public @Nonnull MappingFile findMappingFile(String serverVers) {
+		String[] vp = serverVers.split("\\.");
 
 		int maj, min, pat;
-
 		try {
 			maj = Integer.parseInt(vp[0]);
 			min = Integer.parseInt(vp[1]);
 			pat = vp.length > 2 ? Integer.parseInt(vp[2]) : 0;
 		} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-			plugin.severe("Could not parse server version string: " + serverVersion);
-			return p.getResource("mapping-" + serverVersion + ".json");
+			plugin.severe("Could not parse server version string: " + serverVers);
+			return new MappingFile(serverVers, p.getResource("mapping-" + serverVers + ".json"));
 		}
 
-		plugin.info("Searching for mappings, starting from " + serverVersion + " and going down.");
+		plugin.info("Searching for mappings, starting from " + serverVers + " and going down.");
 
-		for (int m = min; m >= 16; m--) {
-			int sp = (m == min) ? pat : 9;
+		for (int m = min; m >= 0; m--) {
+			int sp = (m == min) ? pat : 11; // if on correct minor version, start from patch, otherwise start from highest possible patch
 
 			for (int pt = sp; pt >= 0; pt--) {
 				String vtt = maj + "." + m + "." + pt;
-				String fn = "mapping-" + vtt + ".json";
+				String fileName = "mapping-" + vtt + ".json";
 
-				InputStream is = p.getResource(fn);
+				InputStream inpStream = p.getResource(fileName);
 
-				if (is != null) {
-					if (!vtt.equals(serverVersion)) {
-						plugin.info("Using fallback mapping file: " + fn);
+				if (inpStream != null) {
+					if (!vtt.equals(serverVers)) {
+						plugin.info("Using fallback mapping file: " + fileName);
 					} else {
-						plugin.info("Found exact mapping file: " + fn);
+						plugin.info("Found exact mapping file: " + fileName);
 					}
-					return is;
+					return new MappingFile(vtt, inpStream);
 				}
 			}
 
-			String mvfn = "mapping-" + maj + "." + m + ".json";
-			InputStream is = p.getResource(mvfn);
-			if (is != null) {
-				plugin.info("Using fallback mapping file: " + mvfn);
-				return is;
+        	// Check for the major.min version without patch
+			String fileName = "mapping-" + maj + "." + m + ".json";
+			InputStream inpStream = p.getResource(fileName);
+			if (inpStream != null) {
+				plugin.info("Using fallback mapping file: " + fileName);
+				return  new MappingFile(maj + "." + m, inpStream);
+			}
+
+			// Switch to 1.21.x versions after 26.0
+			if (maj >= 26 && m == 0) {
+				maj = 1;
+				m = 22; // will be 21 in the next iteration
 			}
 		}
 
-		plugin.severe("Could not find any suitable mapping file after checking all versions down to " + maj + ".16.0");
-		return null;
+		plugin.severe("Could not find any suitable mapping file after checking all versions down to 1.0.0");
+		return new MappingFile(serverVers, null);
 	}
 
-	private void generateMappings(File f) {
-		try (InputStream is = fmf()) {
-			if (is == null) {
-				plugin.severe("Failed to find mapping-" + serverVersion + ".json in plugin resources!");
+	private void generateMappings() {
+		try {
+			MappingFile mapFile = findMappingFile(serverVersion);
+			if (mapFile.stream == null) {
+				plugin.severe("Failed to find mapping file for " + serverVersion + " in plugin resources!");
 				return;
 			}
 
-			ObjectMapper m = new ObjectMapper();
+			ObjectMapper mapper = new ObjectMapper();
 			@SuppressWarnings("unchecked")
-			Map<String, Object> r = m.readValue(is, Map.class);
+			Map<String, Object> r = mapper.readValue(mapFile.stream, Map.class);
+			mapFile.stream.close();
 			@SuppressWarnings("unchecked")
-			List<String> s = (List<String>) r.get("blockstates");
+			List<String> states = (List<String>) r.get("blockstates");
 
-			if (s == null) {
+			if (states == null) {
 				plugin.severe("'blockstates' key not found in JSON.");
 				return;
 			}
 
-			Object2ObjectOpenHashMap<String, int[]> nlm = new Object2ObjectOpenHashMap<>();
-			plugin.info("Generating legacy mappings for " + s.size() + " block states...");
+			Object2ObjectOpenHashMap<String, int[]> newLegacyMappings = new Object2ObjectOpenHashMap<>();
+			plugin.info("Generating legacy mappings for " + states.size() + " block states...");
 
-			for (int i = 0; i < s.size(); i++) {
-				String k = s.get(i).replace("minecraft:", "");
+			ProtocolVersion serverProto = ProtocolVersion.getClosest(mapFile.version); // start from base mappings file version
+			ProtocolVersion clientProto = ProtocolVersion.v1_12_2;
+
+			List<ProtocolPathEntry> protoPath = Via.getManager()
+				.getProtocolManager()
+				.getProtocolPath(clientProto, serverProto);
+
+			if (protoPath == null) {
+				plugin.log(Level.SEVERE, "Protocol path is null!");
+				return;
+			}
+
+			for (int i = 0; i < states.size(); i++) {
+				String k = states.get(i).replace("minecraft:", "");
 				String blockName = k.contains("[") ? k.substring(0, k.indexOf("[")) : k;
 
 				int[] legacy;
@@ -179,43 +202,43 @@ public class ViaBlockIds {
 						legacy = new int[]{146, 0};
 						break;
 					default:
-						legacy = ctl(i);
+						legacy = convertToLegacy(protoPath, i);
 						break;
 				}
 
-				nlm.put(k, legacy);
+				newLegacyMappings.put(k, legacy);
 			}
 
-			legacyMappings = nlm;
+			legacyMappings = newLegacyMappings;
 
-			Map<String, Object> o = new Object2ObjectOpenHashMap<>();
-			o.put("blockstates", legacyMappings);
+			Map<String, Object> outputMap = new Object2ObjectOpenHashMap<>();
+			outputMap.put("blockstates", legacyMappings);
 
-			f.getParentFile().mkdirs();
-			m.writerWithDefaultPrettyPrinter().writeValue(f, o);
-			plugin.info("Successfully wrote mappings to " + f.getName());
+			mappingsFile.getParentFile().mkdirs();
+			mapper.writerWithDefaultPrettyPrinter().writeValue(mappingsFile, outputMap);
+			plugin.info("Successfully wrote mappings to " + mappingsFile.getName());
 
 		} catch (Exception e) {
 			plugin.log(Level.SEVERE, "Error generating legacy mappings.", e);
 		}
 	}
 
-	private void loadMappings(File f) {
+	private void loadMappings() {
 		try {
-			ObjectMapper m = new ObjectMapper();
+			ObjectMapper mapper = new ObjectMapper();
 			@SuppressWarnings("unchecked")
-			Map<String, Object> r = m.readValue(f, Map.class);
+			Map<String, Object> r = mapper.readValue(mappingsFile, Map.class);
 			@SuppressWarnings("unchecked")
-			Map<String, List<Integer>> rm = (Map<String, List<Integer>>) r.get("blockstates");
+			Map<String, List<Integer>> readMap = (Map<String, List<Integer>>) r.get("blockstates");
 
-			if (rm == null) {
+			if (readMap == null) {
 				plugin.severe("Invalid format in mappings file. Regenerating...");
-				generateMappings(f);
+				generateMappings();
 				return;
 			}
 
 			legacyMappings = new Object2ObjectOpenHashMap<>();
-			for (Map.Entry<String, List<Integer>> e : rm.entrySet()) {
+			for (Map.Entry<String, List<Integer>> e : readMap.entrySet()) {
 				String fullKey = e.getKey();
 				List<Integer> ll = e.getValue();
 
@@ -246,36 +269,22 @@ public class ViaBlockIds {
 		}
 	}
 
-	public int[] ctl(int mbsi) {
-		ProtocolVersion serverProto = Via.getAPI().getServerVersion().highestSupportedProtocolVersion();
-		ProtocolVersion clientProto = ProtocolVersion.v1_12_2;
-
-		List<ProtocolPathEntry> protoPath = Via.getManager()
-			.getProtocolManager()
-			.getProtocolPath(clientProto, serverProto);
-
-		if (protoPath == null) {
-			plugin.log(Level.WARNING, "Protocol path is null!");
-			return new int[]{1, 0};
-		}
-
-		int csi = mbsi;
-
+	public int[] convertToLegacy(List<ProtocolPathEntry> protoPath, int stateId) {
 		for (int i = protoPath.size() - 1; i >= 0; i--) {
-			ProtocolPathEntry e = protoPath.get(i);
-			Protocol<?, ?, ?, ?> pr = e.protocol();
+			ProtocolPathEntry entry = protoPath.get(i);
+			Protocol<?, ?, ?, ?> protocol = entry.protocol();
 
-			if (pr instanceof BackwardsProtocol) {
-				BackwardsMappingData md = ((BackwardsProtocol<?, ?, ?, ?>) pr).getMappingData();
-				if (md != null && md.getBlockStateMappings() != null) {
-					int ni = md.getBlockStateMappings().getNewId(csi);
+			if (protocol instanceof BackwardsProtocol) {
+				BackwardsMappingData mappingData = ((BackwardsProtocol<?, ?, ?, ?>) protocol).getMappingData();
+				if (mappingData != null && mappingData.getBlockStateMappings() != null) {
+					int newStateId = mappingData.getBlockStateMappings().getNewId(stateId);
 
-					if (ni != -1) csi = ni;
+					if (newStateId != -1) stateId = newStateId;
 				}
 			}
 		}
-		int bi = csi >> 4;
-		int mt = csi & 0xF;
-		return new int[]{bi, mt};
+		int blockId = stateId >> 4;
+		int blockMetadata = stateId & 0xF;
+		return new int[]{blockId, blockMetadata};
 	}
 }
